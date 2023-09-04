e5c268e649b5405e6e97bac5ee97cc3eada5bc75
Joni Hackzell <t1hajo05@students.oamk.fi>
2023-09-04 11:47:08 +0300

    updated

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [Age, setAge] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput placeholder='Enter age'
+       keyboardType='number-pad'
+       value={Age}
+        onChangeText={text => setAge(text)}/>
+      <Text>Heart Rate Limit</Text>
+      <Text>{((220-Age)*0.65).toFixed(0)} - {((220-Age)*0.85).toFixed(0)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +23,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
   },
 });
